c83c09f0b663bca3237c04eaf1f7363c4f75a85a
Thiago Correa da Fonseca <thiagocorreafonseca@gmail.com>
2024-02-21 22:55:06 -0300

    Teste Tipos e Variaveis

--- FundamentosCSharp.Console/FundamentosCSharp.Console/Program.cs
+++ FundamentosCSharp.Console/FundamentosCSharp.Console/Program.cs
@@ -4,3 +4,26 @@ Console.WriteLine("Hello, World!");
 var nome = Console.ReadLine();
 
 Console.WriteLine(nome);
+
+double valorDouble = 3.14;
+float valorFloat = 3.14F;
+decimal valorDecimal = 3.14M;
+
+int valorInteiro = 100;
+bool valorBoolean = true;
+char valorChar = 'A';
+string valorString = "Thiago";
+
+const double PI = 3.1415;
+
+Console.WriteLine($"Double: {valorDouble}");
+Console.WriteLine($"Float: {valorFloat}");
+Console.WriteLine($"Int: {valorInteiro}");
+Console.WriteLine($"Decimal: {valorDecimal}");
+Console.WriteLine($"---------");
+Console.WriteLine($"Boolean: {valorBoolean}");
+Console.WriteLine($"Char: {valorChar}");
+Console.WriteLine($"String: {valorString}");
+Console.WriteLine($"---------");
+Console.WriteLine($"CONST: {PI}");
+
